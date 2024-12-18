e2ff9d7fca7f7c67914f9b5694567be0aac8c160
ADarko22 <a.elessar22@gmail.com>
2024-12-18 20:45:01 +0100

    Day 15 - Cleanup

--- solutions/src/main/kotlin/org/example/Day15.kt
+++ solutions/src/main/kotlin/org/example/Day15.kt
@@ -12,117 +12,40 @@ class Day15 : Day {
     private val input = "day15".readResourceLines()
 
     override fun part1() {
-        val (warehouse, movements) = parseInput()
-        warehouse.moveRobot(movements)
+        val (warehouse, movements) = parseInput(doubleShapes = false)
+        warehouse.moveRobotInVisualMode(movements)
         val solution = warehouse.getBoxPositions().sumOf { it.toGoodsPositioningSystemLocation() }
         println("Day 15 part 1: $solution")
     }
 
     override fun part2() {
-        val (bigWarehouse, movements) = parseInput2()
-        bigWarehouse.moveRobot(movements)
+        val (bigWarehouse, movements) = parseInput(doubleShapes = true)
+        bigWarehouse.moveRobotInVisualMode(movements)
         val solution = bigWarehouse.getBoxPositions().sumOf { it.toGoodsPositioningSystemLocation() }
         println("Day 15 part 2: $solution")
     }
 
-    private data class Warehouse(val grid: Array<Array<Char>>) {
-        val rows = grid.size
-        val cols = grid[0].size
-        var robotPosition: Position = robotPosition()
-
-        private fun robotPosition(): Position {
-            for (row in 0 until rows)
-                for (col in 0 until cols)
-                    if (grid[row][col] == '@')
-                        return Position(row, col)
-            throw IllegalStateException("Robot not found in warehouse")
-        }
-
-        fun moveRobot(movements: List<Movement>) = movements.forEach { moveRobot(it) }
-
-
-        private fun moveRobot(movement: Movement) {
-            val newRobotPosition = robotPosition.move(movement)
-
-            when {
-                isFreeSpace(newRobotPosition) -> {
-                    grid[robotPosition.row][robotPosition.col] = '.'
-                    grid[newRobotPosition.row][newRobotPosition.col] = '@'
-                    robotPosition = newRobotPosition
-                }
-
-                isBox(newRobotPosition) -> {
-                    // try to move the robot and all the boxes
-                    // if there is a free space in front of the last consecutively placed box
-                    var lastBoxPosition = newRobotPosition
-                    while (isBox(lastBoxPosition)) {
-                        val newBoxPosition = lastBoxPosition.move(movement)
-
-                        if (isFreeSpace(newBoxPosition)) {
-                            grid[newBoxPosition.row][newBoxPosition.col] = 'O'
-                            grid[robotPosition.row][robotPosition.col] = '.'
-                            grid[newRobotPosition.row][newRobotPosition.col] = '@'
-                            robotPosition = newRobotPosition
-                            break
-                        }
-
-                        lastBoxPosition = newBoxPosition
-                    }
-                }
-            }
-        }
-
-        private fun isFreeSpace(position: Position) = grid[position.row][position.col] == '.'
-
-        private fun isBox(position: Position) = grid[position.row][position.col] == 'O'
-
-        fun getBoxPositions(): List<Position> {
-            val boxPositions = mutableListOf<Position>()
-            for (row in 0 until rows)
-                for (col in 0 until cols)
-                    if (grid[row][col] == 'O')
-                        boxPositions.add(Position(row, col))
-            return boxPositions
-        }
-    }
-
     private enum class Movement(val value: Char, val row: Int, val col: Int) {
         NONE('.', 0, 0),
-        NORTH('^', -1, 0),
-        SOUTH('v', 1, 0),
-        EAST('>', 0, 1),
-        WEST('<', 0, -1);
+        UP('^', -1, 0),
+        DOWN('v', 1, 0),
+        RIGHT('>', 0, 1),
+        LEFT('<', 0, -1);
 
         companion object {
             fun fromValue(value: Char) =
                 entries.find { it.value == value } ?: throw IllegalArgumentException("Invalid movement value: $value")
         }
 
-        fun isHorizontal() = this == EAST || this == WEST
+        fun isHorizontal() = this == RIGHT || this == LEFT
     }
 
     private data class Position(val row: Int, val col: Int) {
-        fun move(movement: Movement): Position {
-            return Position(row + movement.row, col + movement.col)
-        }
-
+        fun move(movement: Movement) = Position(row + movement.row, col + movement.col)
         fun toGoodsPositioningSystemLocation(): Long = 100L * row + col
     }
 
-    private fun parseInput(): Pair<Warehouse, List<Movement>> {
-        return input.indexOf("")
-            .let { splitInputIdx ->
-                val warehouseGrid = input.subList(0, splitInputIdx)
-                    .map { it.toCharArray().toTypedArray() }
-                    .toTypedArray()
-                val movements = input.subList(splitInputIdx + 1, input.size)
-                    .joinToString("")
-                    .map { Movement.fromValue(it) }
-                Pair(Warehouse(warehouseGrid), movements)
-            }
-    }
-
-    private data class BigWarehouse(val grid: Array<Array<Char>>) {
+    private data class Warehouse(val grid: Array<Array<Char>>) {
         val rows = grid.size
         val cols = grid[0].size
         var robotPosition: Position = robotPosition()
@@ -139,31 +62,27 @@ class Day15 : Day {
         private fun moveRobot(movement: Movement) {
             val newRobotPosition = robotPosition.move(movement)
 
-            if (isFreeSpace(newRobotPosition)
-                || (isBox(newRobotPosition) && tryMoveBoxes(newRobotPosition, movement))
-            ) {
+            val boxesMoved = isAnyBox(newRobotPosition) && tryMoveBox(newRobotPosition, movement)
+
+            if (isFreeSpace(newRobotPosition) || boxesMoved) {
                 grid[robotPosition.row][robotPosition.col] = '.'
                 grid[newRobotPosition.row][newRobotPosition.col] = '@'
                 robotPosition = newRobotPosition
             }
         }
 
-        private fun tryMoveBoxes(boxInitialPosition: Position, movement: Movement): Boolean {
-            val boxInitialLeftSidePosition =
-                if (grid[boxInitialPosition.row][boxInitialPosition.col] == '[') boxInitialPosition
-                else boxInitialPosition.move(Movement.WEST)
+        private fun tryMoveBox(boxPosition: Position, movement: Movement) =
+            if (movement.isHorizontal()) tryMoveBoxesHorizontally(boxPosition, movement)
+            else tryMoveBoxesVertically(boxPosition, movement)
 
-            return if (movement.isHorizontal()) tryMoveBoxesHorizontally(boxInitialPosition, movement)
-            else tryMoveBoxesVertically(boxInitialLeftSidePosition, movement)
-        }
+        private fun tryMoveBoxesHorizontally(boxPosition: Position, movement: Movement): Boolean {
+            var lastBoxPosition = adjustPositionForBigBox(boxPosition)
 
-        private fun tryMoveBoxesHorizontally(boxInitialPosition: Position, movement: Movement): Boolean {
-            var lastBoxPosition = boxInitialPosition
-            while (isBox(lastBoxPosition)) {
+            while (isAnyBox(lastBoxPosition)) {
                 val newBoxPosition = lastBoxPosition.move(movement)
 
                 if (isFreeSpace(newBoxPosition)) {
-                    moveBoxesHorizontally(movement, boxInitialPosition.col, newBoxPosition.col, lastBoxPosition.row)
+                    moveBoxesHorizontally(movement, boxPosition.col, newBoxPosition.col, lastBoxPosition.row)
                     return true
                 }
 
@@ -174,28 +93,34 @@ class Day15 : Day {
 
         private fun moveBoxesHorizontally(movement: Movement, startCol: Int, finalCol: Int, row: Int) =
             when (movement) {
-                Movement.EAST -> for (col in finalCol downTo startCol) grid[row][col] = grid[row][col - 1]
-                Movement.WEST -> for (col in finalCol until startCol) grid[row][col] = grid[row][col + 1]
+                Movement.RIGHT -> for (col in finalCol downTo startCol) grid[row][col] = grid[row][col - 1]
+                Movement.LEFT -> for (col in finalCol until startCol) grid[row][col] = grid[row][col + 1]
                 else -> throw IllegalArgumentException("Invalid movement for horizontal box movement")
             }
 
-        // boxInitialPosition is the left side of the box
-        private fun tryMoveBoxesVertically(boxInitialPosition: Position, movement: Movement): Boolean {
-            val boxesToMovePerRow = mutableMapOf<Int, Set<Position>>()
-            boxesToMovePerRow[boxInitialPosition.row] = mutableSetOf(boxInitialPosition)
+        private fun tryMoveBoxesVertically(boxPosition: Position, movement: Movement): Boolean {
+            val boxStartPosition = adjustPositionForBigBox(boxPosition)
 
-            var currentRow = boxInitialPosition.row
+            // DFS to find all boxes touched
+            val boxesToMovePerRow = mutableMapOf<Int, Set<Position>>()
+            boxesToMovePerRow[boxStartPosition.row] = mutableSetOf(boxStartPosition)
+
+            var currentRow = boxStartPosition.row
             var nextRow = currentRow + movement.row
 
-            while (!isBlockedByWallRowForBoxes(nextRow, boxesToMovePerRow[currentRow]!!)) {
-                if (isFreeRowForBoxes(nextRow, boxesToMovePerRow[currentRow]!!)) {
-                    moveBoxesVertically(movement, startRow = boxInitialPosition.row, nextRow, boxesToMovePerRow)
+            while (!areBoxesBlockedOnNextRow(nextRow, boxesToMovePerRow[currentRow]!!)) {
+
+                if (areBoxesFreeOnNextRow(nextRow, boxesToMovePerRow[currentRow]!!)) {
+                    moveBoxesVertically(
+                        movement, boxesToMovePerRow, isBigBox(boxStartPosition),
+                        startRow = boxStartPosition.row, finalRow = nextRow
+                    )
                     return true
                 }
 
-                val nextRowBoxes = boxesToMovePerRow[currentRow]!!
-                    .map { it.col }.flatMap { boxesToMove(nextRow, it) }.toSet()
-                boxesToMovePerRow[nextRow] = nextRowBoxes
+                boxesToMovePerRow[nextRow] = boxesToMovePerRow[currentRow]!!.map { it.col }
+                    .flatMap { boxesToMoveVertically(nextRow, it) }
+                    .toSet()
                 currentRow = nextRow
                 nextRow = currentRow + movement.row
             }
@@ -203,8 +128,9 @@ class Day15 : Day {
             return false
         }
 
-        private fun boxesToMove(nextRow: Int, it: Int): List<Position> {
+        private fun boxesToMoveVertically(nextRow: Int, it: Int): List<Position> {
             val nextRowBoxes = mutableListOf<Position>()
+            if (grid[nextRow][it] == 'O') nextRowBoxes.add(Position(nextRow, it))
             if (grid[nextRow][it] == '[') nextRowBoxes.add(Position(nextRow, it))
             if (grid[nextRow][it] == ']') nextRowBoxes.add(Position(nextRow, it - 1))
             if (grid[nextRow][it + 1] == '[') nextRowBoxes.add(Position(nextRow, it + 1))
@@ -212,49 +138,52 @@ class Day15 : Day {
         }
 
         private fun moveBoxesVertically(
-            movement: Movement,
-            startRow: Int,
-            finalRow: Int,
-            boxesToMovePerRow: MutableMap<Int, Set<Position>>
+            movement: Movement, boxesToMovePerRow: MutableMap<Int, Set<Position>>, isBigBox: Boolean,
+            startRow: Int, finalRow: Int
         ) {
-            if (movement == Movement.SOUTH) {
-                for (row in finalRow downTo startRow + 1) {
-                    for (col in boxesToMovePerRow[row - 1]!!.map { it.col }) {
-                        grid[row][col] = grid[row - 1][col]
-                        grid[row - 1][col] = '.'
-                        grid[row][col + 1] = grid[row - 1][col + 1]
-                        grid[row - 1][col + 1] = '.'
-                    }
-                }
-            } else {
-                for (row in finalRow until startRow) {
-                    for (col in boxesToMovePerRow[row + 1]!!.map { it.col }) {
-                        grid[row][col] = grid[row + 1][col]
-                        grid[row + 1][col] = '.'
-                        grid[row][col + 1] = grid[row + 1][col + 1]
-                        grid[row + 1][col + 1] = '.'
+            val rows = if (movement == Movement.DOWN) finalRow downTo startRow + 1 else finalRow until startRow
+
+            for (row in rows) {
+                val previousRow = row - movement.row
+
+                for (boxPosition in boxesToMovePerRow[previousRow]!!) {
+                    grid[row][boxPosition.col] = grid[previousRow][boxPosition.col]
+                    grid[previousRow][boxPosition.col] = '.'
+
+                    if (isBigBox) {
+                        grid[row][boxPosition.col + 1] = grid[previousRow][boxPosition.col + 1]
+                        grid[previousRow][boxPosition.col + 1] = '.'
                     }
                 }
             }
         }
 
-        private fun isBlockedByWallRowForBoxes(row: Int, boxLeftSidePositions: Set<Position>) =
-            boxLeftSidePositions.any { grid[row][it.col] == '#' || grid[row][it.col + 1] == '#' }
+        private fun areBoxesBlockedOnNextRow(row: Int, boxPositions: Set<Position>) =
+            boxPositions.any { grid[row][it.col] == '#' || (isBigBox(it) && grid[row][it.col + 1] == '#') }
 
-        private fun isFreeRowForBoxes(row: Int, boxLeftSidePositions: Set<Position>) =
-            boxLeftSidePositions.all { grid[row][it.col] == '.' && grid[row][it.col + 1] == '.' }
+        private fun areBoxesFreeOnNextRow(row: Int, boxPositions: Set<Position>) =
+            boxPositions.all { grid[row][it.col] == '.' && (!isBigBox(it) || grid[row][it.col + 1] == '.') }
 
         private fun isFreeSpace(position: Position) = grid[position.row][position.col] == '.'
 
-        private fun isBox(position: Position) =
+        private fun isAnyBox(position: Position) = isBox(position) || isBigBox(position)
+
+        private fun isBox(position: Position) = grid[position.row][position.col] == 'O'
+
+        private fun isBigBox(position: Position) =
             grid[position.row][position.col] == '[' || grid[position.row][position.col] == ']'
+
+        // if the box is a BigBox, the position is adjusted to the left side
+        private fun adjustPositionForBigBox(boxPosition: Position) =
+            if (grid[boxPosition.row][boxPosition.col] == ']') boxPosition.move(Movement.LEFT)
+            else boxPosition
 
         fun getBoxPositions(): List<Position> {
             val boxPositions = mutableListOf<Position>()
             for (row in 0 until rows)
                 for (col in 0 until cols)
-                // identify the box position by the left side
-                    if (grid[row][col] == '[')
+                // The BigBoxes are identified by '['
+                    if (grid[row][col] == '[' || grid[row][col] == 'O')
                         boxPositions.add(Position(row, col))
             return boxPositions
         }
@@ -264,10 +193,9 @@ class Day15 : Day {
             printWarehouse(Movement.NONE, 0.0, clearScreen = false)
             movements.forEachIndexed { index, movement ->
                 moveRobot(movement)
-                if (!movement.isHorizontal()) {
-                    val progress = ((index + 1) * 100.0) / movements.size
-                    printWarehouse(movement, progress)
-                }
+                val progress = ((index + 1) * 100.0) / movements.size
+                printWarehouse(movement, progress)
+
             }
         }
 
@@ -285,6 +213,7 @@ class Day15 : Day {
                     when (cell) {
                         '@' -> "${red}${cell}${reset}"
                         '.' -> "${green}${cell}${reset}"
+                        'O' -> "${blue}${cell}${reset}"
                         '[' -> "${blue}${cell}${reset}"
                         ']' -> "${blue}${cell}${reset}"
                         else -> cell
@@ -297,23 +226,28 @@ class Day15 : Day {
             print("\r$cursorUp$progressAsString\n$gridString\n")
             TimeUnit.MILLISECONDS.sleep(1000)
         }
-
     }
 
-    private fun parseInput2(): Pair<BigWarehouse, List<Movement>> {
-        val (warehouse, movements) = parseInput()
-
-        val bigWarehouseGrid = Array(warehouse.rows) { row ->
-            Array(warehouse.cols * 2) { col ->
-                when (warehouse.grid[row][col / 2]) {
-                    '@' -> if (col % 2 == 0) '@' else '.'
-                    '#' -> '#'
-                    'O' -> if (col % 2 == 0) '[' else ']'
-                    else -> '.'
-                }
+    private fun parseInput(doubleShapes: Boolean): Pair<Warehouse, List<Movement>> {
+        return input.indexOf("")
+            .let { splitInputIdx ->
+                val warehouseGrid = input.subList(0, splitInputIdx)
+                    .map {
+                        if (doubleShapes)
+                            it.replace("O", "[]")
+                                .replace("#", "##")
+                                .replace(".", "..")
+                                .replace("@", "@.")
+                        else it
+                    }.map {
+                        it.toCharArray()
+                            .toTypedArray()
+                    }
+                    .toTypedArray()
+                val movements = input.subList(splitInputIdx + 1, input.size)
+                    .joinToString("")
+                    .map { Movement.fromValue(it) }
+                Pair(Warehouse(warehouseGrid), movements)
             }
-        }
-
-        return Pair(BigWarehouse(bigWarehouseGrid), movements)
     }
 }
