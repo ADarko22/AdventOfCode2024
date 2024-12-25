a858f917cc7d8f7d0d240a3123fb5ef5d900f1d6
ADarko22 <a.elessar22@gmail.com>
2024-12-25 10:59:03 +0100

    Day 25 - p1

---
+++ solutions/src/main/kotlin/org/example/Day25.kt
@@ -0,0 +1,61 @@
+package org.example
+
+fun main() {
+    Day25().part1()
+    Day25().part2()
+}
+
+class Day25 : Day {
+
+    private val input = "day25".readResourceLines()
+
+    override fun part1() {
+        val solution = countUniqueFittingLockKeyPairs()
+        println("Day 25 part 1: $solution")
+    }
+
+    override fun part2() {
+        val (locks, keys) = parseInput()
+        val solution = "todo"
+        println("Day 25 part 2: $solution")
+    }
+
+    private fun countUniqueFittingLockKeyPairs(): Int {
+        val (locks, keys) = parseInput()
+        return keys.sumOf { key -> locks.count { lock -> lock.matches(key) } }
+    }
+
+    private data class Lock(val pinHeights: List<Int>) {
+        fun matches(key: Key) = pinHeights.zip(key.pinHeights).all { (lockPin, keyPin) -> lockPin + keyPin <= 5 }
+    }
+
+    private data class Key(val pinHeights: List<Int>)
+
+    private fun parseInput(): Pair<List<Lock>, List<Key>> {
+        val locks = mutableListOf<Lock>()
+        val keys = mutableListOf<Key>()
+
+        var idx = 0
+        while (idx < input.size) {
+            if (input[idx] == "#####") {
+                val lockPins = List(size = 5) { 0 }.toMutableList()
+
+                for (col in 0 until 5)
+                    for (row in idx + 1..idx + 5)
+                        if (input[row][col] == '#') lockPins[col]++
+
+                locks.add(Lock(lockPins))
+            } else {
+                val keyPins = List(size = 5) { 0 }.toMutableList()
+
+                for (col in 0 until 5)
+                    for (row in idx + 5 downTo idx + 1)
+                        if (input[row][col] == '#') keyPins[col]++
+
+                keys.add(Key(keyPins))
+            }
+            idx += 8
+        }
+        return locks to keys
+    }
+}
